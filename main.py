2e4e8b9fa833a831e5f32d21f83a50a3e102256a
Woobean Lee <w.lee@gatech.edu>
2021-04-05 21:12:37 +0900

    Add CNN example

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ print('size of data :  ', len(data))
 #print('data["data"] : ', data['data'])
 for each_column in data:
     print('each columns name : ', each_column, ', size : ', len(data[each_column]))
-np_data = np.array(data['data']) # For converting to a NumP
+
+np_data = np.array(data['data']) # For converting to a NumPy
 print(type(np_data))
+
 for np_column in np_data[0][0]:
     print('each np_columns size : ', len(np_column))
-print(np_data[0][0][0])
+print(np_data[0][0])
